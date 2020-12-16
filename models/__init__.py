641019f3100b0a06a9a9df8d4c0fb427051cc812
juan <juan@magraojj>
2020-12-16 12:51:13 -0300

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.7
+#coding: utf-8
+
+##########################################################################################################
+#    
+#    NOTA: ESTE PROGRAMA SERA PARTE INTEGRANTE DO SISTEMA C.A.I.O PARA I.A NO MODULO RUSSELL RPA; 
+#    @AUTHOR: SANDRO REGIS CARDOSO
+##########################################################################################################
+
+from . import CalculusPrime
---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.7
+#coding: utf-8
+
+##########################################################################################################
+#    
+#    NOTA: ESTE PROGRAMA SERA PARTE INTEGRANTE DO SISTEMA C.A.I.O PARA I.A NO MODULO RUSSELL RPA; 
+#    @AUTHOR: SANDRO REGIS CARDOSO
+##########################################################################################################
+
+from . import main_test
+from . import sphinx
+from . import analyse_prime_sequence
